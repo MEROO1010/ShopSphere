148b22d9007714f5c001c89c0bce54e5500704a2
unknown <meroo1995saggaf@outlook.com>
2025-05-29 13:41:40 +0300

    start the project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
